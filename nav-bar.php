e8161e88a70eae289e86259218cd9e1dfef68897
ju1618 <juliaefernandez@gmail.com>
2019-04-22 16:55:50 -0300

    fondo claro y tipografia

---
+++ nav-bar.php
@@ -0,0 +1,12 @@
+<?php
+<img class="logo" src="images/logo1.png" alt="logo tu empresa">
+<nav class="main-nav">
+  <ul>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Productos</a></li>
+    <li><a href="faqs.html">Preguntas Frequentes</a></li>
+    // info de contacto
+  </ul>
+    <li><a href="#">Perfil</a></li> /*del lasdo de sign in y log in*/
+</nav>
+?>
