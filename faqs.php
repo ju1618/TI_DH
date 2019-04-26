1fbbf54fe835cbc113f90a91961575fe316129a4
ju1618 <juliaefernandez@gmail.com>
2019-04-26 18:02:57 -0300

    modificaciones en navbar gral y navbar-faq

--- faqs.php
+++ faqs.php
@@ -10,67 +10,94 @@
 
   </head>
   <body>
+
     <?php require_once('nav-bar.php'); ?>
+
     <div class="container">
-      <section class="cd-faq faq-temas navbar-nav-scroll ">
-       <ul class="cd-faq-categories categorias navbar-nav bd-navbar-nav">
-          <li class="nav-item"><a href="#sesion">Configuración de cuenta</a></li>
-          <li class="nav-item"><a href="#venta">Venta</a></li>
-          <li class="nav-item"><a href="#compra">Compra</a></li>
-       </ul>
-      </section>
+      <nav class="navbar navbar-expand-sm navbar-expand-md navbar-dark faq-temas">
+        <button class="navbar-toggler" type="button" >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse">
+           <ul class="navbar-nav mr-auto categorias">
+            <li class="nav-item">
+              <a class="nav-link" href="#sesion">Configuración de cuenta</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#publicacion">Publicar</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#compra">Compra</a>
+            </li>
+         </ul>
+        </div>
+      </nav>
 
        <div class="cd-faq-items">
-          <ul id="sesion" class="cd-faq-group">
-             <li class="cd-faq-title"><h2>Configuración de cuenta</h2></li>
-             <li>
-                <a class="cd-faq-trigger" href="#0"><strong>¿Cómo cambio mi contraseña?</strong></a>
-                <div class="cd-faq-content">
-                   RESPUESTA.
-                   Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </div>
-                <br>
-             </li>
+         <ul>
+             <li class="cd-faq-title cd-faq-group" id="sesion"><h2>Configuración de cuenta</h2></li>
+               <li>
+                  <div class="cd-faq-trigger pregunta" href="#0"><h4>¿Cómo crear un usuario nuevo?</h4></div>
+                  <div class="cd-faq-content respuesta">
+                    Podés crear una cuenta rápidamente haciendo <a class="nav-link"  href="#registrarse">click acá</a>.
+                    <br>La única información que te vamos a pedir es una cuenta de correo electrónico válida y que elijas una contraseña.
+                    <br>Luego de ingresar la información, te vamos a enviar un correo con un enlace para que puedas activar tu cuenta.
+                    <br>Si necesitas ayuda con alguno de los pasos, contactanos.
+                  </div>
+                  <br>
+               </li>
+               <li>
+                  <div class="cd-faq-trigger pregunta" href="#0"><h4>¿Cómo cambio mi contraseña?</h4></div>
+                  <div class="cd-faq-content respuesta">
+                    ¡Modificar tu contraseña es muy fácil! Sólo tenés que seguir estos simples pasos:
+                    <ol>
+                      <li>Si es que ya no iniciaste sesión, ingresá a tu <a class="nav-link"  href="#ingresar">cuenta</a>.</li>
+                      <li>Una vez que estés dentro de tu cuenta, hacé clic en "Mi Perfil".</li>
+                      <li>En el campo "Contraseña" hacé clic en "Cambiar".</li>
+                      <li>Ingresá y confirmá tu nueva contraseña, hacé clic en "Enviar" y ¡Listo!.</li>
+                    </ol>
+                    <br>Si luego de realizar todos los pasos, no pudiste cambiar tu contraseña, por favor contactate con nuestro equipo de Soporte.
+                  </div>
+                  <br>
+               </li>
 
-             <li>
-                <a class="cd-faq-trigger" href="#0"><strong>¿Cómo crear un usuario nuevo?</strong></a>
-                <div class="cd-faq-content">
-                   RESPUESTA.
-                   Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </div>
-                <br>
-             </li>
-
-
-          </ul>
-
-          <ul id="venta" class="cd-faq-group">
-            <li class="cd-faq-title"><h2>Venta</h2></li>
+            <li class="cd-faq-title cd-faq-group" id="publicacion"><h2>Publicación</h2></li>
             <li>
-              <a class="cd-faq-trigger" href="#0"><strong>¿Cómo publico un producto?</strong></a>
-              <div class="cd-faq-content">
-                RESPUESTA.
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-             </div>
+              <div class="cd-faq-trigger pregunta" href="#0"><h4>¿Cómo publico un producto?</h4></div>
+              <div class="cd-faq-content respuesta">
+                Para poder publicar y gestionar tus avisos correctamente, necesitás registrar tu correo.
+
+                Si aún no lo hiciste, Ingresá al siguiente <a class="nav-link" href="#registro">link</a> y completá los datos.
+                <strong>Pasos para publicar tu anuncio:</strong>
+                <ol>
+                  <li>Seleccioná "Ingresar" en el menú de navegación, en el margen superior, y completá los campos con tu mail y contraseña.</li>
+                  <li>Hacé clic en el botón "Vender".</li>
+                  <li>Seleccioná la categoría a la que corresponde tu producto o servicio.</li>
+                  <li>Añadí un título, fotos, y una descripción, contale a todos por qué tu anuncio es diferente. No olvides verificar tu información personal para que los compradores puedan contactarte.</li>
+                  <li>Agregá fotos de buena calidad, así como también un título y descripción atractivos.</li>
+                  <li>Hacé clic en "Publicar" al final de la página.</li>
+                  <li>¡Ya casi está listo! Tu anuncio va a estar disponible en muy pronto. Te vamos a enviar un email para confirmarte cuando esté online.</li>
+                </ol>
+            </div>
+             <br>
+           </li>
+
+            <li class="cd-faq-title cd-faq-group"id="compra"><h2>Compra</h2></li>
+            <li>
+              <div class="cd-faq-trigger pregunta" href="#0"><h4>¿Cómo elegir a qué vendedor comprarle?</h4></div>
+              <div class="cd-faq-content respuesta">
+                Si estás buscando un <b>servicio</b>, te recomendamos que leas las opiniones de las personas que ya lo contrataron. Y si querés comprar un auto o un inmueble, siempre es bueno llamar al anunciante para coordinar una visita y sacarte todas las dudas sobre lo que querés comprar.
+                <br><strong>Leé en detalle la publicación</strong>
+                <br>Revisá atentamente toda la publicación: prestá atención a la descripción y a las fotos para estar seguro de las condiciones de venta que propone el vendedor.
+                Si el producto es nuevo, asegurate de que tenga garantía y que puedas devolverlo en caso de ser necesario.
+              </div>
              <br>
            </li>
           </ul>
-
-          <ul id="compra" class="cd-faq-group">
-            <li class="cd-faq-title"><h2>Compra</h2></li>
-            <li>
-              <a class="cd-faq-trigger" href="#0"><strong>¿PREGUNTA?</strong></a>
-              <div class="cd-faq-content">
-                RESPUESTA.
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-             </div>
-             <br>
-           </li>
-          </ul>
-
-          <!-- ... -->
        </div>
-<!-- esto lo saque de la internet -->
     </div>
+
+    <?php require_once('footer.php'); ?>
+
   </body>
 </html>
--- nav-bar.php
+++ nav-bar.php
@@ -1,17 +1,19 @@
 <!-- ver navbar de la pag de bootstrap  https://getbootstrap.com/docs/4.3/getting-started/introduction/-->
 
 
- <!--                              mi navbar                       -->
+ <!--                              navbar                       -->
 
-<header class="main-nav navbar navbar-expand navbar-dark flex-column flex-md-row bd-navbar" style="background-color: #4bd1b6">
-  <a class= "navbar-brand mr-0 mr-md-2" href="#">
-    <!-- /*que vaya al home*/ -->
-  <img class="logo navbar-brand" src="images/logo1.png" alt="logo tu empresa">
+<nav class="main-nav navbar navbar-expand-sm navbar-expand-md navbar-dark flex-column flex-md-row bd-navbar" style="background-color: #4bd1b6">
+  <a class= "navbar-brand" href="#HOME">
+    <img class="logo navbar-brand" src="images/logo1.png" alt="logo tu empresa">
   </a>
-  <div class="navbar-nav-scroll">
-    <ul class="navbar-nav bd-navbar-nav flex-row">
+  <button class="navbar-toggler" type="button" data-toggle="collapse">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse">
+    <ul class="navbar-nav mr-auto">
       <li class="nav-item">
-        <a class= "nav-link" href="#">Home</a>
+        <a class= "nav-link" href="#">Home </a>
       </li>
       <li class="nav-item">
         <a class= "nav-link" href="#">Productos</a>
@@ -20,11 +22,15 @@
         <a class= "nav-link" href="faqs.php">FAQ</a>
       </li>
       <li class="nav-item">
+        <a class= "nav-link" href="#">Registrarse</a>
+      </li>
+      <li class="nav-item">
+        <a class= "nav-link" href="#">Ingresar</a>
+      </li>
+      <li class="nav-item">
         <a class= "nav-link" href="#">Perfil</a>
       </li>
       <!-- // info de contacto -->
     </ul>
   </div>
-
-    <!-- /*del lado de sign in y log in*/ -->
-</header>
+</nav>
