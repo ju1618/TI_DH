fa51c106ba6954bd899e3661a320c2e8634e495e
ju1618 <juliaefernandez@gmail.com>
2019-04-27 14:32:22 -0300

    mejorando el responsive de los navs

--- faqs.php
+++ faqs.php
@@ -5,13 +5,14 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/stylefaq.css">
     <link rel="stylesheet" href="css/navstyle.css">
+    <link rel="stylesheet" href="css/stylefaq.css">
 
   </head>
   <body>
 
     <?php require_once('nav-bar.php'); ?>
+    
       <nav class="navbar navbar-expand-sm navbar-expand-md navbar-dark fixed-top faq-temas justify-content-center">
         <button class="navbar-toggler" type="button" >
           <span class="navbar-toggler-icon"></span>
--- nav-bar.php
+++ nav-bar.php
@@ -3,15 +3,15 @@
 
  <!--                              navbar                       -->
 
-<nav class="main-nav navbar navbar-expand-sm navbar-expand-md navbar-dark flex-column flex-md-row bd-navbar fixed-top">
-  <a class= "navbar-brand" href="#HOME">
+<nav class="main-nav navbar fixed-top navbar-light navbar-expand-md">
+  <a class= "navbar-brand" href="index-proyecto.php">
     <img class="logo navbar-brand" src="images/logo1.png" alt="logo tu empresa">
   </a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse">
-    <span class="navbar-toggler-icon"></span>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse">
-    <ul class="navbar-nav mr-auto">
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
       <li class="nav-item active">
         <a class= "nav-link" href="index-proyecto.php">Home </a>
       </li>
@@ -21,16 +21,15 @@
       <li class="nav-item active">
         <a class= "nav-link" href="faqs.php">FAQ</a>
       </li>
-      <li class="nav-item">
+      <!-- <li class="nav-item">
         <a class= "nav-link" href="#">Registrarse</a>
-      </li>
+      </li> -->
       <li class="nav-item" >
-        <a class= "nav-link" href="#">Ingresar</a>
+        <a class= "nav-link" href="login.php">Ingresar</a>
       </li>
       <li class="nav-item">
         <a class= "nav-link" href="perfilusuario.php">Perfil</a>
       </li>
-      <!-- // info de contacto -->
     </ul>
   </div>
 </nav>
