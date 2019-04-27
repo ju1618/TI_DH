9752523b0aa5afcd78b30ccf9a2eb67af509f355
soltq-1990 <lau_nuni@live.com>
2019-04-27 13:06:21 -0300

    Subiendo demo de login

---
+++ login.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Login</title>
+    <meta name="viewport" content="width=device-width, inital-scale=1">
+    <!-- Compiled and minified Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+    <!-- Minified JS library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!-- Compiled and minified Bootstrap JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ></script>
+    <link rel="stylesheet" href="css/styles-login.css">
+  </head>
+  <body>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="hidden-xs col-md-6 col-lg-6 col1">
+          <div id="myCarousel" class="carousel slide" data-ride="carousel">
+              <!-- Wrapper for slides -->
+              <div class="carousel-inner carrousel">
+                <div class="item active">
+                  <img src="img/fondo7.jpg" alt="">
+                </div>
+                <div class="item">
+                  <img src="img/fondo10.jpg" alt="">
+                </div>
+                <div class="item">
+                  <img src="img/fondo.jpg" alt="">
+                </div>
+              </div>
+            </div>
+        </div>
+          <div class="col-sm-12 col-md-6 col2">
+
+              <div class="container login-container">
+                <div class="col-md-12 login-form-1">
+                  <h3>Bienvenido a <bold>market palace!</bold></h3>
+                  <h4>Un lugar donde encontrarás todo lo que buscas</h4>
+                  <h5>Ingresa aqui tus datos!</h5>
+                      <div class="form-group">
+                          <input type="text" class="form-control" placeholder="User" value="" />
+                      </div>
+                      <div class="form-group">
+                          <input type="password" class="form-control" placeholder="Password" value="" />
+                      </div>
+                      <div class="form-group">
+                          <input type="submit" class="btnSubmit" value="Login" />
+                      </div>
+                      <div class="form-group">
+                          <a href="#" class="btnForgetPwd">Forget Password?</a>
+                      </div>
+
+              </div>
+            </div>
+
+            </div>
+          </div>
+          </div>
+              <!-- fin del form -->
+      </div>
+    </div>
+  </div>
+      <!-- fin -->
+  </body>
+</html>
