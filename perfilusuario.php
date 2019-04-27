b42246eaebcd2981611f2844d7bef356544ee8c3
Romi07 <rgramirezuba@gmail.com>
2019-04-27 12:45:34 -0300

    agregue perfilstyle y el perfilusuario.php

---
+++ perfilusuario.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width = device-width, initial-scale = 1">
+    <title>Mi perfil</title>
+    <!--Vinculción externa de CSS-->
+<link rel="stylesheet" href="css/styles.css">
+    <!--Bootstrap CDN-->
+<!--  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">-->
+  </head>
+  <body>
+    <div class="container">
+      <div class="contenedor1">
+        <img width="100" src="img/imagen.png" alt="foto del usuario">
+        <h1>Mi cuenta</h1>
+        <h2>Hola, usuario!</h2>
+        <br>
+        <br>
+        <ul>
+          <li>Mis Datos</li>
+          <br>
+          <li>Compras</li>
+          <br>
+          <li>Facturación</li>
+        </ul>
+      </div>
+      <div class="contenedor2">
+        <ul>
+          <li>Mis datos</li>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,</p>
+        </ul>
+      </div>
+   </div>
+  </body>
+</html>
