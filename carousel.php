378ea65d7f0da46b26a8b7d18a8f38024f166b33
mveron007 <mveron13814@gmail.com>
2019-04-28 15:37:15 -0300

    subo arreglo parcial de carrusel

---
+++ carousel.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="TI_DH/css/style-index.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <meta charset="utf-8">
+    <title></title>
+    <!-- <div id="MagicCarousel" class="carousle-slide" data-ride="carousel">
+      <div class="carousel-inner" role="listbox">
+        <div class="carousel-item">
+          <img class="d-block w-100" src="" alt="First slide">
+
+        </div>
+      </div>
+
+    </div> -->
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="TI_DH/images/logo1.png" class="d-block w-100" alt="First Slide">
+        </div>
+        <div class="carousel-item">
+          <img src="TI_DH/images/office-2.jpg" class="d-block w-100" alt="Second Slide">
+        </div>
+        <div class="carousel-item">
+          <img src="TI_DH/images/cook.jpg" class="d-block w-100" alt="Third Slide">
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+  </head>
+  <body>
+
+  </body>
+</html>
