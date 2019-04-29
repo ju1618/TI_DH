b31d3d493a3a184bb8b517da2af701de28311c0c
mveron007 <mveron13814@gmail.com>
2019-04-29 16:22:32 -0300

    ahora si el index arreglado

--- carousel.php
+++ carousel.php
@@ -4,7 +4,7 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="TI_DH/css/style-index.css">
+    <link rel="stylesheet" href="css/style-index.css">
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
     <meta charset="utf-8">
     <title></title>
@@ -20,17 +20,17 @@
     <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
       <div class="carousel-inner">
         <div class="carousel-item active">
-          <img src="TI_DH/images/logo1.png" class="d-block w-100" alt="First Slide">
+          <img src="images/logo1.png" class="d-block w-100" alt="First Slide">
         </div>
         <div class="carousel-item">
-          <img src="TI_DH/images/office-2.jpg" class="d-block w-100" alt="Second Slide">
+          <img src="images/office-2.jpg" class="d-block w-100" alt="Second Slide">
           <div class="carousel-caption d-none d-md-block">
           <a href="#oficina"> <h1 style="color:white;">Accesorios para tu oficina</h1></a>
           <!-- <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> -->
           </div>
         </div>
         <div class="carousel-item">
-          <img src="TI_DH/images/cook.jpg" class="d-block w-100" alt="Third Slide">
+          <img src="images/cook.jpg" class="d-block w-100" alt="Third Slide">
           <div class="carousel-caption d-none d-md-block">
           <a href="#cocina"> <h1 style="color:white;">Los mejores gadgets para cocinar</h1></a>
           <!-- <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> -->
--- index-proyecto2.php
+++ index-proyecto2.php
@@ -4,8 +4,8 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="TI_DH/css/style-index.css">
-    <link rel="stylesheet" href="TI_DH/css/navstyle.css">
+    <link rel="stylesheet" href="css/style-index.css">
+    <link rel="stylesheet" href="css/navstyle.css">
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="utf-8">
@@ -16,7 +16,7 @@
 
 
       <header>
-        <?php require_once("TI_DH/nav-bar.php");
+        <?php require_once("nav-bar.php");
         ?>
         <!-- <nav class="navbar navbar-expand-sm navbar-expand-md navbar-dark fixed-top faq-temas justify-content-center">
           <button class="navbar-toggler" type="button" >
@@ -38,7 +38,7 @@
         </nav> -->
       </header>
       <br><br>
-      <?php require_once("TI_DH/carousel.php") ?>
+      <?php require_once("carousel.php") ?>
 
 
       <div id="cocina" class="main-content-product">
@@ -53,7 +53,7 @@
         </div>
         <div class="row" id="content-product">
           <div class="col-sm-12 col-md-2 col-lg-4">
-          <img src="TI_DH/images/molde-frog.jpg" alt="molde-frog">
+          <img src="images/molde-frog.jpg" alt="molde-frog">
           <h2>Molde de rana</h2>
           <a href="#"><span>Detalles</span></a>
           </div>
@@ -61,7 +61,7 @@
         </div>
         <div class="row" id="content-product">
           <div class="col-sm-1 col-md-2 col-lg-4">
-          <img src="TI_DH/images/cucharon-dino.jpg" alt="cucharon">
+          <img src="images/cucharon-dino.jpg" alt="cucharon">
           <h2>Cucharon de dinosaurio</h2>
           <a href="#"><span>Detalles</span></a>
           </div>
@@ -70,7 +70,7 @@
 
         <div class="row" id="content-product">
           <div class="col-sm-1 col-md-2 col-lg-4">
-          <img src="TI_DH/images/rociador-limon.jpg" alt="rociador">
+          <img src="images/rociador-limon.jpg" alt="rociador">
           <h2>Rociador de limón</h2>
           <a href="#"><span>Detalles</span></a>
           </div>
@@ -78,7 +78,7 @@
         </div>
         <div class="row" id="content-product">
           <div class="col-sm-1 col-md-2 col-lg-4">
-          <img src="TI_DH/images/esparcidor-unicornio.jpg" alt="unicornio">
+          <img src="images/esparcidor-unicornio.jpg" alt="unicornio">
           <h2>Esparcidor de confites</h2>
           <a href="#"><span>Detalles</span></a>
           </div>
@@ -86,7 +86,7 @@
         </div>
         <div class="row" id="content-product">
           <div class="col-sm-1 col-md-2 col-lg-4">
-          <img src="TI_DH/images/molde-huevo.jpg" alt="molde">
+          <img src="images/molde-huevo.jpg" alt="molde">
           <h2>Molde de corazón</h2>
           <a href="#"><span>Detalles</span></a>
           </div>
@@ -100,7 +100,7 @@
 
         <div class="row" id="content-product">
           <div class="col-sm-1 col-md-2 col-lg-4">
-          <img src="TI_DH/images/mesa-pizarra.jpg" alt="mesa-pizarra">
+          <img src="images/mesa-pizarra.jpg" alt="mesa-pizarra">
           <h2>Mesa pizarra</h2>
           <a href="#"><span>Detalles</span></a>
           </div>
@@ -108,7 +108,7 @@
         </div>
         <div class="row" id="content-product">
           <div class="col-sm-12 col-md-2 col-lg-4">
-          <img src="TI_DH/images/anotador-head.jpg" alt="anotador-head">
+          <img src="images/anotador-head.jpg" alt="anotador-head">
           <h2>Anotador de cabeza</h2>
           <a href="#"><span>Detalles</span></a>
           </div>
@@ -116,7 +116,7 @@
         </div>
         <div class="row" id="content-product">
           <div class="col-sm-1 col-md-2 col-lg-4">
-          <img src="TI_DH/images/basket-cesto.jpg" alt="basket-cesto">
+          <img src="images/basket-cesto.jpg" alt="basket-cesto">
           <h2>Cesto Basket-Office</h2>
           <a href="#"><span>Detalles</span></a>
           </div>
@@ -125,7 +125,7 @@
 
         <div class="row" id="content-product">
           <div class="col-sm-1 col-md-2 col-lg-4">
-          <img src="TI_DH/images/cup-holder.jpg" alt="cup-holder">
+          <img src="images/cup-holder.jpg" alt="cup-holder">
           <h2>Sostén de taza</h2>
           <a href="#"><span>Detalles</span></a>
           </div>
@@ -133,7 +133,7 @@
         </div>
         <div class="row" id="content-product">
           <div class="col-sm-1 col-md-2 col-lg-4">
-          <img src="TI_DH/images/apoya-pies.jpg" alt="apoya-pies">
+          <img src="images/apoya-pies.jpg" alt="apoya-pies">
           <h2>Apoya pies de escritorio</h2>
           <a href="#"><span>Detalles</span></a>
           </div>
@@ -141,7 +141,7 @@
         </div>
         <div class="row" id="content-product">
           <div class="col-sm-1 col-md-2 col-lg-4">
-          <img src="TI_DH/images/aire-usb.jpg" alt="aire-usb">
+          <img src="images/aire-usb.jpg" alt="aire-usb">
           <h2>Aire frio/calor USB</h2>
           <a href="#"><span>Detalles</span></a>
           </div>
@@ -160,7 +160,7 @@
         <a href="#">  <img src="images/top-button.png" alt="to-top"></a>
       </div> -->
       <footer>
-       <?php require_once("TI_DH/footer.php"); ?>
+       <?php require_once("footer.php"); ?>
       </footer>
 
     </div>
