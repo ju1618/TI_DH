f829d03d5c4a1a8fe882321736f1226d9cba71da
Romi07 <rgramirezuba@gmail.com>
2019-04-29 05:09:58 -0300

    modificacion de perfilstyle y perfilusuario

--- perfilusuario.php
+++ perfilusuario.php
@@ -2,7 +2,7 @@
 <html lang="es" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width = device-width, initial-scale = 1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Mi perfil</title>
     <!--Vinculción externa de CSS-->
 <link rel="stylesheet" href="css/perfilstyles.css">
@@ -12,15 +12,17 @@
   </head>
   <body>
       <?php require_once('nav-bar.php'); ?>
-    <div class="container">
-      <div class="contenedor1">
-        <center><img class="fotousuario" src="images/imagen.png" alt="foto del usuario"></center>
-         <br>
-         <br>
-        <h1>Mi cuenta</h1>
-        <h2>Hola, Tomás!!!</h2>
-        <br>
-        <br>
+    <div class="contenedorprincipal">
+       <div class="contenedor1">
+          <br>
+          <center><img class="fotousuario" src="images/imagen.png" alt="foto del usuario"></center>
+          <br>
+          <br>
+                <h1><center>Mi cuenta</center></h1>
+                <br>
+            <h2>Hola, Tomás!!!</h2>
+          <br>
+          <br>
           <a href="#MisDatos">Mis Datos</a>
           <br>
           <br>
@@ -29,9 +31,12 @@
           <br>
           <a href="#facturas">Facturación</a>
           <br>
+          <br>
+          <a href="#cambiopass">Cambiar Contraseña</a>
       </div>
       <div class="contenedor2">
-          <h2 id = "MisDatos"><em><strong>Mis datos</strong></em></li></h2>
+          <br>
+          <h2 id = "MisDatos"><em><strong><center>Mis datos</center></strong></em></li></h2>
           <br>
           <p><strong>Nombre y Apellido:</strong> Tomás Gomez</p>
           <br>
@@ -43,15 +48,22 @@
           <br>
       </div>
       <div class="contenedor3">
-        <h2 id="compras"><em><strong>Compras</strong></em></li></h2>
-        <img class="productocomprado" src="images/mate-de-madera-colorido.jpg" alt="producto comprado">
+        <br>
+        <h2 id="compras"><em><strong><center>Compras</center></strong></em></li></h2>
+        <br>
+        <center><img class="productocomprado" src="images/mate-de-madera-colorido.jpg" alt="producto comprado"></center>
         <p>Mate de madera colorido</p>
         <p>Cantidad: 2</p>
         <p>Precio: $250</p>
         <p>Total: 2 x $250 = $500</p>
         <br>
       </div>
+      <div class="contenedor4">
+        <br>
+        <h2 id="facturas"><em><strong><center>Facturación</center></strong></em></li></h2>
+        <img class="facturadecompra" src="images/facturamp.png" alt="factura de compra">
+      </div>
    </div>
    <?php require_once('footer.php'); ?>
-  </body>
+ </body>
 </html>
