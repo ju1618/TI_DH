0fdc79d6338a434cc7e0e688085f40d0d30c55fa
ju1618 <juliaefernandez@gmail.com>
2019-05-17 13:11:48 -0300

    limpié el css del detalle de producto

--- detalle-producto.php
+++ detalle-producto.php
@@ -25,23 +25,19 @@
                       <div class="col-xs-12 col-sm-12 col-md-6 offset-md-3 mi-detalle-contenedor">
                             <!-- Como si fuera el 'header' del detalle -->
                             <div class="mi-cabecera-detalle">
-                              <div class="titulo">
-                                    <h3> Detalle de Producto</h3>
-                              </div>
+                                <h3> Detalle de Producto</h3>
                             </div>
                             <!-- Aca va a ir el contenido en si       -->
                             <div class="mi-principal-detalle">
-                              <div class="nombre-producto">
-                                <h4>Mate con diseño</h4>
-                              </div>
+                              <!--       Titulo       -->
+                              <h4 class="text-center">Mate con diseño</h4>
                               <!--       imagen       -->
-                              <img src="images/mate-de-madera-colorido.jpg" class="img-fluid imagen-producto" alt="mate-diseño">
+                              <img src="images/mate-de-madera-colorido.jpg" class="img-fluid rounded mx-auto imagen-producto" alt="mate-diseño">
                               <!--       detalle        -->
                               <div class="">
                                 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-
                               </div>
-                              <!--          boton voler          -->
+                              <!--          botón voler          -->
                               <a class=" btn btn-primary mi-boton" href="index-proyecto2.php">Volver</a>
                           </div>
                       </div>
