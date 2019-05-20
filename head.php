9642f8b39316108b4f39fb50d542b6c7d9b71c5e
ju1618 <juliaefernandez@gmail.com>
2019-05-20 16:04:02 -0300

    modifique el footer, agregué mapita

--- head.php
+++ head.php
@@ -8,5 +8,6 @@
   <link href="https://necolas.github.io/normalize.css/8.0.1/normalize.css" rel="stylesheet">
   <!--         link a bootstrap        -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <!--                links a hojas de estilo nav-bar            -->
+  <!--                links a hojas de estilo nav-bar y footer           -->
   <link rel="stylesheet" href="css/navstyle.css">
+  <link rel="stylesheet" href="css/styles-footer.css">
